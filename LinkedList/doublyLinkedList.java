67a7b9505069cb916f4017b7728a96bcbe0eb41b
Abhishek <Abhishek-Singh1111>
2025-09-03 14:14:02 +0530

    some changes in my file

--- LinkedList/doublyLinkedList.java
+++ LinkedList/doublyLinkedList.java
@@ -2,38 +2,43 @@ public class doublyLinkedList{
      public class Node {
         int data;
         Node next;
+        Node prev;
+        Node(int data){
+            this.data = data;
+            this.next = null;
+            this.prev = null;
+        }
     }
-    Node head;
-    Node tail;
+    Node head=null;
+    Node tail=null;
     Node prev;
-     public void adddoublyLinkedList(int data){
-        Node newNode = new Node();
-        newNode.data = data;
+     public void adddoubly(int data){
+        Node newNode = new Node(data);
         newNode.next = null;
         if(head == null){
             head = newNode;
             tail = newNode;
-            prev = null;
         }else{
             tail.next = newNode;
             prev = tail;
+            newNode.prev = tail;
             tail = newNode;
         }
      }
         public void display(){
             Node current = head;
-            System.out.print("null<-");
+            System.out.print("null of prev<-");
             while(current != null){
                 System.out.print(current.data+"-> ");
                 current = current.next;
             }
-            System.out.println("null");
+            System.out.println("null of next" );
         }
          public static void main(String[] args) {
         doublyLinkedList list = new doublyLinkedList();
-        list.adddoublyLinkedList(1);
-        list.adddoublyLinkedList(2);
-        list.adddoublyLinkedList(4);
+        list.adddoubly(1);
+        list.adddoubly(2);
+        list.adddoubly(4);
         list.display();
     }
     
